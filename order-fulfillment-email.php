047cbaae6a354333af2413e67ffc98fa0558001e
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-03-16 14:53:02 -0700

    dialing in the template

--- order-fulfillment-email.php
+++ order-fulfillment-email.php
@@ -3,7 +3,7 @@
 $debug = false;
 $webhookContent = json_decode(file_get_contents('php://input'), true);
 $server_location = 'http://gabesimagination.com/myshirtbin-app/';
-include('includes/PHPMailer/class.phpmailer.php');
+require 'includes/PHPMailer/PHPMailerAutoload.php';
 //order number
 	if(isset($webhookContent['order_number'])){
 		$order_number = $webhookContent['order_number'];
@@ -281,5 +281,45 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: orders@myshirtbin.com' . "\r\n";
 
 // Mail it
-mail($to, $subject, $message, $headers);
+//mail($to, $subject, $message, $headers);
+
+//PHP MAILER https://github.com/Synchro/PHPMailer
+if(isset($webhookContent['order_number']) || htmlspecialchars($_GET["test"] == 1)){
+	error_log(print_r($webhookContent, true));
+$mail = new PHPMailer;
+
+//$mail->isSMTP();                                      // Set mailer to use SMTP
+//$mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup server
+//$mail->SMTPAuth = true;                               // Enable SMTP authentication
+//$mail->Username = 'jswan';                            // SMTP username
+//$mail->Password = 'secret';                           // SMTP password
+//$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
+
+$mail->From = 'orders@myshirtbin.com';
+$mail->FromName = 'My Shirt Bin Orders';
+$mail->addAddress('fulfill@myshirtbin.com');  // Add a recipient
+$mail->addReplyTo('orders@myshirtbin.com', 'Orders');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+$mail->WordWrap = 50;                                 // Set word wrap to 50 characters
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = $subject;
+$mail->Body    = $message;
+$mail->AltBody = 'Please view this email in HTML mode, it\'s going to looka  lot better. '.strip_tags($message);
+
+if(!$mail->send()) {
+   echo 'Message could not be sent.';
+   echo 'Mailer Error: ' . $mail->ErrorInfo;
+   exit;
+}
+
+echo 'Message has been sent';
+}
+else {
+	echo 'Error bad request';
+}
 ?>
