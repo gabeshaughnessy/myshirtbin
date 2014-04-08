c292d3c6b6401709cc3cef85ee5406259cd8d82c
gabeshaughnessy <gabeshaughnessy@gmail.com>
2014-04-08 14:23:48 -0700

    no autop

--- wp-content/themes/Myshirtbin/functions.php
+++ wp-content/themes/Myshirtbin/functions.php
@@ -1,6 +1,8 @@
 <?php
-/* Replace WordPress Logo on login screen */
 
+remove_filter('the_content', 'wpautop');
+
+/* Replace WordPress Logo on login screen */
 function msb_login_logo() {
 ?> <style type="text/css">
 #login h1 a {background: url(<?php bloginfo('stylesheet_directory'); ?>/images/logo.svg) no-repeat; background-size: 326px 90px; width: 326px !important; height: 90px !important;}
